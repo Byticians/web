2fa9717884ff66f2fcee93fb1bec90f5e57c53bd
roshansourav <roshansouravj@gmail.com>
2020-09-20 09:36:33 +0530

    initial commit for v2 final Byticians website

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$('.navbar-nav a').on('click', function(){
+    $('.btn-navbar').click(); //bootstrap 2.x
+    $('.navbar-toggle').click(); //bootstrap 
+    $('.navbar-toggler').click(); //bootstrap 4.x
+});
